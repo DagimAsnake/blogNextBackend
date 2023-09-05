259a51c6f28d24d9628a80c1854adc8e46ba91a2
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-05 02:23:27 -1200

    added get profile

--- control/profile.js
+++ control/profile.js
@@ -38,3 +38,15 @@ module.exports.ChangePassword = async function (req, res) {
         msg: "Password Changed Successfully"
     }).status(200)
 }
+
+module.exports.viewProfile =async (req, res) => {
+    const token = req.get("Authorization").split(" ")[1];
+    let decodedToken = jwt.verify(token, SECRET_KEY);
+
+    const user = await User.findById(decodedToken.id)
+    if (!user) {
+        return res.json({ msg: "User not found" }).status(403)
+    }
+
+    return res.json({ msg: user }).status(200)
+}
--- route/profile.js
+++ route/profile.js
@@ -3,5 +3,6 @@ const profileController = require("../control/profile")
 const isLoggedIn = require("../util/isLoggedIn");
 
 router.put('/changepassword', isLoggedIn, profileController.ChangePassword)
+router.get("/", isLoggedIn, profileController.viewProfile)
 
 module.exports = router;
